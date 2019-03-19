86c672e663589cd0eaf5baa08a6205fa3f49504a
SHIVAM KR TIWARI <shivamtind@gmail.com>
2019-03-19 15:29:14 +0530

    towards single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,22 +2,35 @@ import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishdetailComponent from "./DishdetailComponent";
 import Footer from "./FooterComponent";
 import { Dishes } from "../shared/dishes";
+import { Comments } from "../shared/comments";
+import { Leaders } from "../shared/leaders";
+import { Promotions } from "../shared/promotions";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: Dishes
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -30,7 +43,8 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
-          <Redirect to="home" />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </div>

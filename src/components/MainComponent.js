caa4aad084ca7781d4e771dcc346c51ecceabe2e
SHIVAM KR TIWARI <shivamtind@gmail.com>
2019-06-10 17:53:47 +0530

    apdate to fetch leaders data from server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,7 +12,8 @@ import {
   postComment,
   fetchDishes,
   fetchComments,
-  fetchPromos
+  fetchPromos,
+  fetchLeaders
 } from "../redux/ActionCreators";
 import { actions } from "react-redux-form";
 
@@ -26,7 +27,8 @@ const mapDispatchToProps = dispatch => ({
     dispatch(actions.reset("feedback"));
   },
   fetchComments: () => dispatch(fetchComments()),
-  fetchPromos: () => dispatch(fetchPromos())
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders())
 });
 
 const mapStateToProps = state => {
@@ -47,6 +49,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
 
   render() {
@@ -61,7 +64,11 @@ class Main extends Component {
           }
           promoLoading={this.props.promotions.isLoading}
           promoErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.filter(leader => leader.featured)[0]}
+          leader={
+            this.props.leaders.leaders.filter(leader => leader.featured)[0]
+          }
+          leadersLoading={this.props.leaders.isLoading}
+          leaderErrMess={this.props.leaders.errMess}
         />
       );
     };
